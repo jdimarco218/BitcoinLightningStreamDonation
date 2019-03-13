bf41ebcfbd8dff439b99202ddc0be9495f38b6b3
jdimarco218 <jdimarco218@gmail.com>
2019-03-12 23:44:25 -0400

    First working skips on our songs based on sats paid!

--- client/src/RequestsService.js
+++ client/src/RequestsService.js
@@ -73,56 +73,6 @@ class RequestsService {
         }
     }
 
-    // Insert caption
-    static async insertCaption(text, apiKey){
-        var captionAmount = baseCost;
-        text = xssFilters.inHTMLData(text);
-        var costRes = await new Promise(async (res, rej) => {
-            try {
-                var costRes = await axios.get(captionCostUrl);
-                console.log('res from cost url: ');
-                console.log(costRes.data);
-                captionAmount = costRes.data;
-                res(costRes.data);
-
-            } catch(err) {
-                rej(err);
-            }
-        });
-        console.log(`captionAmount: ${captionAmount.data}`);
-        if (captionAmount.data === 'undefined') {
-            captionAmount.data = baseCost;
-        }
-        console.log(`OPENNODE_APIKEY: ${process.env.OPENNODE_APIKEY}`);
-        console.log(`opennodeAuth; ${opennodeAuth}`);
-        console.log(`apiKey: ${apiKey}`);
-        var headers = {
-                'Content-Type': 'application/json',
-                'Authorization': `${process.env.OPENNODE_APIKEY}`
-        };
-        console.log(`creating caption with port: ${port}`);
-        var body = `{ \"amount\": ${captionAmount}, \"callback_url\": \"http://${host}:${port}/api/posts/update/captions\" }`;
-
-        console.log(`time to do some caption lightning!`);
-        console.log(`AUTHcap: ${process} `);
-        return axios.post('https://api.opennode.co/v1/charges', body, {headers: headers})
-        .then(function (response) {
-            if (response.status === 201) {
-                console.log(`new charge_id: ${response.data.data.id}`);
-                axios.post(captionUrl, {
-                    text: text,
-                    charge_id: response.data.data.id
-                });
-            }
-            console.log(response);
-            console.log(`insertCaption returning: ${response.data.data.lightning_invoice.payreq}`);
-            return response.data.data.lightning_invoice.payreq;
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-    }
-
     // Delete Post
     static deletePost(id){
         return axios.delete(`${url}${id}`);
@@ -131,12 +81,6 @@ class RequestsService {
     static async getImageCost(){
         var result = await axios.get(postCostUrl);
         console.log(`getImageCost: ${JSON.stringify(result.data)}`);
-        return JSON.stringify(result.data);
-    }
-
-    static async getCaptionCost(){
-        var result = await axios.get(captionCostUrl);
-        console.log(`getCaptionCost: ${JSON.stringify(result.data)}`);
         return JSON.stringify(result.data);
     }
 }
--- server/routes/api/requests.js
+++ server/routes/api/requests.js
@@ -27,7 +27,8 @@ router.post('/', async(req, res) => {
         paidAt: createdAt,
         charge_id: req.body.charge_id,
         paid: false,
-        amountPaid: 0
+        amountPaid: 0,
+        se_id: ''
     });
     res.status(201).send();
 });
@@ -44,6 +45,7 @@ router.post('/update', async(req, res) => {
     for (var i = requestsArray.length - 1; notFound && i >= 0; i--) {
         if (requestsArray[i].hasOwnProperty("charge_id")) {
             const chargeUrl = 'https://dev-api.opennode.co/v1/charge/' + requestsArray[i]["charge_id"];
+            var ourChargeId = '';
             await axios.get(chargeUrl, { headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'bd5ecb21-6fba-4cfa-949c-a5c70149ad27'
@@ -51,6 +53,7 @@ router.post('/update', async(req, res) => {
             .then(response => {
                 if (response.status === 200 &&
                     response.data.data.status === 'paid') {
+                    ourChargeId = response.data.data.id;
                     responseVal = requestsArray[i].video;
                     notFound = false;
                     var paidAt = new Date();
@@ -73,16 +76,30 @@ router.post('/update', async(req, res) => {
             console.log(`newRequestUrl: ${newRequestUrl}`);
             console.log(`curr JWT: ${streamElementsJWT}`);
             console.log(`body: ${body}`);
+            var receivedStreamElementsID = '';
             await axios.post(newRequestUrl, body, { headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${streamElementsJWT}`
             }})
-            .then(response => {
+            .then(seResponse => {
+                receivedStreamElementsID = seResponse.data._id;
                 console.log(`Successfully queued: ${responseVal}`);
             })
             .catch((error) => {
                 console.log(`New request error: ${error}`);
             });
+
+            //
+            // Keep the ID that StreamElements uses to refer to later
+            //
+            if (receivedStreamElementsID !== '') {
+                requests.findOneAndUpdate(
+                    { "charge_id": ourChargeId },
+                    { $set: {
+                        "se_id": receivedStreamElementsID  } },
+                    { upsert: true}
+                );
+            }
         }
     }
     console.log("Done.");
@@ -118,7 +135,9 @@ async function loadRequestsCollection() {
 var prevSongId = '';
 var currSongId = '';
 var currDuration = 0;
+var durationToPlay = 0;
 setInterval(async() => {
+    var isNewSong = false;
     const getSongInfoUrl = 'https://api.streamelements.com/kappa/v2/songrequest/5c86b7740ba7952f06384482/playing';
     await axios.get(getSongInfoUrl, { headers: {
         'Content-Type': 'application/json',
@@ -127,9 +146,15 @@ setInterval(async() => {
     .then(response => {
         currSongId = response.data._id;
         if (currSongId != prevSongId) {
+            //
+            // Reset our counter and current song. We will play this one for the calculated duration
+            // if it exists in our database and has a paid amount. Otherwise, let it play entirely
+            //
+            console.log(`New song detected! Resetting currDuration to 0`);
             currDuration = 0;
             prevSongId = currSongId;
-            console.log(`New song detected! Resetting currDuration to 0`);
+            isNewSong = true;
+
         } else {
             currDuration += 1; // Seconds of this interval
             console.log(`currDuration: ${currDuration}`);
@@ -138,6 +163,44 @@ setInterval(async() => {
     .catch((error) => {
         console.log(`Playing info error: ${error}`);
     });
+    if (isNewSong) {
+        currDuration = 0;
+        var dbReqs = await loadRequestsCollection();
+        //var currSongDb = dbReqs.find(ObjectId(currSongId));
+        console.log(`new song id: ${new mongodb.ObjectID(currSongId)}`);
+        var dbSong = await dbReqs.findOne({"se_id": currSongId});
+        console.log(`db output: ${dbSong}`);
+        if (dbSong !== null) {
+            console.log("This song is from our db!");
+            console.log(`amountPaid: ${dbSong.amountPaid}`);
+            durationToPlay = dbSong.amountPaid;
+        } else {
+            console.log("This new song is not ours.");
+            durationToPlay = 0;
+        }
+    }
+    isNewSong = false;
+
+    //
+    // Here is where we handle the duration paid for. Let's
+    // skip once the number of seconds passes the amount paid
+    // for (multiplied by the rate TODO)
+    //
+    if (durationToPlay != 0 &&
+        currDuration >= durationToPlay) {
+        console.log("Attempting to skip this song now due to duration...");
+        const skipSongUrl = 'https://api.streamelements.com/kappa/v2/songrequest/5c86b7740ba7952f06384482/skip';
+        await axios.post(skipSongUrl, {}, { headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${streamElementsJWT}`
+        }})
+        .then(response => {
+            console.log(`we skipped the latest song!: ${response}`);
+        })
+        .catch(error => {
+            console.log(`Error skipping song: ${error}`);
+        });
+    }
 }, 1000);
 
 module.exports = router;
