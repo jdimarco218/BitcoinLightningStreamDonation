7a86cb249c809baa2cdc2103812d2227f4b63768
jdimarco218 <jdimarco218@gmail.com>
2019-03-14 17:14:52 -0400

    First attempt at building for AWS

---
+++ client/vue.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const port = process.env.VUE_APP_SERVER_PORT || 8085;
+const host = process.env.VUE_APP_SERVER_HOST || "69.141.47.76";
+
+module.exports = {
+    pwa: {
+        iconPaths: {
+            favicon32: 'src/assets/favicon.png'
+        }
+    },
+    outputDir: path.resolve(__dirname, '../server/public'),
+    devServer: {
+        proxy: {
+            '/api': {
+                target: `http://${host}:${port}`
+            }
+        },
+        https: true,
+    }
+}
